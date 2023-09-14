e1f0e19a8e310a03d7762c98f02f860fdf81b709
Jcones20 <joshcones1253@yahoo.com>
2023-09-14 13:38:32 -0700

    python

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,12 +2,12 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    j = 0
+    i = 0
     try:
-        while j is not x:
+        while i is not x:
             print(my_list[i], end='')
-            j += 1
+            i += 1
     except IndexError:
         None
     print()
-    return j
+    return i
