acc0c666855e7de565f9d9862b0bf279af226e40
Jcones20 <joshcones1253@yahoo.com>
2023-09-17 13:22:05 -0700

    python

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,34 +1,37 @@
-
-def roman_value(prmCharacter):
-    roman_list = [('I', 1), ('V', 5), ('X', 10),
-                  ('L', 50), ('C', 100), ('D', 500), ('M', 1000)]
-    for item in roman_list:
-        character, value = item
-        if (prmCharacter is character):
-            return value
-    return None
-
-
-def next_value(prmString, prmIndex):
-    if prmIndex + 1 < len(prmString):
-        return roman_value(prmString[prmIndex + 1])
-    else:
-        return None
-
-
+#!/usr/bin/python3
 def roman_to_int(roman_string):
-    result = 0
-
-    if (roman_string is None or isinstance(roman_string, str) is False):
-        return result
-
-    enum = enumerate(roman_string)
-    for index, character in enum:
-        currentValue = roman_value(character)
-        nextValue = next_value(roman_string, index)
-        if nextValue is None or currentValue >= nextValue:
-            result += currentValue
-        else:
-            result += (nextValue - currentValue)
-            next(enum)
-    return result
+    if type(roman_string) == str:
+        roman_ref = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                     'M': 1000}
+        sum = 0
+        flag = 0
+        for i in range(len(roman_string)):
+            if flag:
+                flag = 0
+                continue
+            for key in roman_ref:
+                if roman_string[i] == key and i != len(roman_string) - 1:
+                    if roman_string[i] == 'I' and roman_string[i + 1] == 'V':
+                        sum += 4
+                        flag = 1
+                    elif roman_string[i] == 'I' and roman_string[i + 1] == 'X':
+                        sum += 9
+                        flag = 1
+                    elif roman_string[i] == 'X' and roman_string[i + 1] == 'L':
+                        sum += 40
+                        flag = 1
+                    elif roman_string[i] == 'X' and roman_string[i + 1] == 'C':
+                        sum += 90
+                        flag = 1
+                    elif roman_string[i] == 'C' and roman_string[i + 1] == 'D':
+                        sum += 400
+                        flag = 1
+                    elif roman_string[i] == 'C' and roman_string[i + 1] == 'M':
+                        sum += 900
+                        flag = 1
+                    else:
+                        sum += roman_ref[key]
+                elif roman_string[i] == key:
+                    sum += roman_ref[key]
+        return sum
+    return 0
