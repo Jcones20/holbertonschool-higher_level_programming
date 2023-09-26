983bd423531ce941149045edf1aacea95568264b
Jcones20 <joshcones1253@yahoo.com>
2023-09-25 21:54:39 -0700

    python

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""Rectangle math.
-
-Contains a class Rectangle that inherits from
-BaseGeometry and some methods.
-"""
+"""module for Rectangle, class what inherits from JC - no test cases"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Defines the Rectangle class that inherits from BaseGeometry."""
-
+    """class rectangle is a subclass of BG"""
     def __init__(self, width, height):
-        """Checks and sets the default attributes of Rectangle class."""
+        """init method"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
