61ebf026984055c13acb0b5e8becd0801d52999e
Jcones20 <joshcones1253@yahoo.com>
2023-09-27 21:08:33 -0700

    python

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
-"""Square file.
+class BaseGeometry:
 
-Contains a class Square that inherits from
-Rectangle and some methods.
-"""
-Rectangle = __import__('9-rectangle').Rectangle
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+        self.integer_validator('width', width)
+        self.integer_validator("height", height)
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__width * self.__height
 
 
 class Square(Rectangle):
-    """Defines the Square class that inherits from Rectangle."""
 
     def __init__(self, size):
-        """Checks and sets the default attributes of Square class."""
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self.__size = size
