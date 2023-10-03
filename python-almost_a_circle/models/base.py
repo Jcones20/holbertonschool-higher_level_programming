7d9d374919f15d51cd28685441c82327c077ef5a
Jcones20 <joshcones1253@yahoo.com>
2023-10-03 09:02:06 -0700

    python

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Module for Base class.'''
+
+
+class Base:
+    '''A representation of the base of our OOP hierarchy.'''
+
+    _nb_objects = 0
+
+    def _init_(self, id=None):
+        '''Constructor.'''
+        if id is not None:
+            self.id = id
+        else:
+            Base._nb_objects += 1
+            self.id = Base._nb_objects
